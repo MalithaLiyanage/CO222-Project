07d98a4c046aa079fc94d6ce24c6353e04c750e8
MalithaLiyanage <malitharanu82@gmail.com>
2018-06-18 15:49:13 +0530

    Done Inputiing and Storing the word list

--- First.c
+++ First.c
@@ -2,24 +2,22 @@
 #include <string.h>
 
 
-char firstRowPuzzle[4];
-char secondRowPuzzle[4];
-char thirdRowPuzzle[4];
-char fourthRowPuzzle[4];
 
-char word1[30];
-char word2[30];
+
 
 int impossible = 0;
-
 int matrix[10][10];
+char words[10][10];
+int sizeOfthePuzzle;
+int noOfWords = 0;
+int x;
+int y = 0;
 
 
 
 
 int get_inputs();
-void turn_to_capital();
-void understanding_Puzzle();
+
 void print_matrix();
 
 
@@ -28,11 +26,11 @@ int main()
 {
 
 	get_inputs();
-	turn_to_capital();  // Turning simples to capitals
 	
-	understanding_Puzzle();
-
+	
+	
 	print_matrix();
+
 
 	printf("Yes\n"); 
 
@@ -43,129 +41,146 @@ int main()
 int get_inputs()
 {
 
+	int enterPressed = 0;
+	int flag1 = 0;
+	int noLine = 0;
+	int isWord = 1;
+	
+	
 
-	scanf("%s %s %s %s %s %s", firstRowPuzzle,secondRowPuzzle,thirdRowPuzzle,fourthRowPuzzle,word1,word2);  // Getting the Inputs
+
+	char line[10];
+
+
+
+	while(enterPressed != 2)
+	{
+
+
+		fgets(line,sizeof(line),stdin);
+
+
+
+
+		if (line[0] == '\n')
+		{
+			enterPressed++;
+		}
+
+		else
+		{
+			if (enterPressed == 1 )
+			{
+				noOfWords++;
+			}
+
+			x = noOfWords;
+
+			if (flag1 == 0)
+			{
+				flag1 = 1;
+				sizeOfthePuzzle = strlen(line) - 1;
+
+				for (int m = 0; m < sizeOfthePuzzle; m++)
+				{
+					for (int n = 0; n < sizeOfthePuzzle; n++)
+					{
+						matrix[m][n] = 2;                           // Giving 2 for the all values of the 10 x 10 matrix
+					}
+				}
+			}
+
+
+			for (int n = 0; n < sizeOfthePuzzle; n++)
+			{
+				if (line[n] >= 97 && line[n] <= 122)     // Turning to Capitals
+				{
+					line[n] = line[n] - 32;
+				}
+
+			}
+
+			for (int n = 0; n < sizeOfthePuzzle; n++)
+				{
+					if (line[n] >= 65 && line[n] <= 90)
+					{
+						isWord = 1;
+					}
+					else
+					{
+						isWord = 0;
+					}	
+
+				}
+
+
+			for (int n = 0; n < sizeOfthePuzzle; n++)
+				{
+					if (line[n] == 35)
+					{
+						matrix[noLine][n] = 1;
+					}                                          // Creating the matrix 1 for vacants 0 for non vacant
+					else 
+					{
+						matrix[noLine][n] = 0;
+					}
+
+				}
+
+				noLine ++;	
+
+		}
+
+
+		if (enterPressed == 1 || enterPressed == 2)
+		{
+			strcpy(words[y],line);
+			y++;
+		}
+
+	}
+
+	
+
+
+	return 0;
 
 
 }
-
-
-void turn_to_capital()
-{
-
-	for(int n = 0, m = 0; n < strlen(word1), m <strlen(word2);n++,m++)
-	{
-		if (word1[n] >= 97 && word1[n] <= 122 )
-		{
-			word1[n] = word1[n] - 32;
-		}
-
-		if (word2[m] >= 97 && word2[m] <= 122)
-		{
-			word2[m] = word2[m] - 32;
-		}
-
-	}
-
-}
-
-void understanding_Puzzle()
-{
-
-	int sizeOfThePuzzle = strlen(firstRowPuzzle);
-	int matrix[sizeOfThePuzzle][sizeOfThePuzzle];
-
-
-	for (int n = 0; n < strlen(firstRowPuzzle); n++)
-	{
-		if (firstRowPuzzle[n] == 42)
-		{
-			matrix[1][n] = 0;
-		}
-		else if (firstRowPuzzle[n] == 35)
-		{
-			matrix[1][n] = 1;
-		}
-		else
-		{
-			impossible = 1;
-		}
-
-	}
-
-	for (int n = 0; n < strlen(secondRowPuzzle); n++)
-	{
-		if (secondRowPuzzle[n] == 42)
-		{
-			matrix[1][n] = 0;
-		}
-		else if (secondRowPuzzle[n] == 35)
-		{
-			matrix[1][n] = 1;
-		}
-		else
-		{
-			impossible = 1;
-		}
-
-	}
-
-	for (int n = 0; n < strlen(thirdRowPuzzle); n++)
-	{
-		if (thirdRowPuzzle[n] == 42)
-		{
-			matrix[1][n] = 0;
-		}
-		else if (thirdRowPuzzle[n] == 35)
-		{
-			matrix[1][n] = 1;
-		}
-		else
-		{
-			impossible = 1;
-		}
-
-	}
-
-	for (int n = 0; n < strlen(fourthRowPuzzle); n++)
-	{
-		if (fourthRowPuzzle[n] == 42)
-		{
-			matrix[1][n] = 0;
-		}
-		else if (fourthRowPuzzle[n] == 35)
-		{
-			matrix[1][n] = 1;
-		}
-		else
-		{
-			impossible = 1;
-		}
-
-	}
 
 	 
-
-
-
-}
 
 void print_matrix()
 {
 
-	for (int m = 0; m < 4; m++)
+	printf("Size of the matrix is  =  %d\n",sizeOfthePuzzle);
+	printf("%d\n%d\n%d\n",noOfWords,x,y);
+
+	for (int m = 0; m < sizeOfthePuzzle; m++)
 	{
-		for (int n = 0; n < strlen(firstRowPuzzle); n++)
+		for (int n = 0; n < sizeOfthePuzzle; n++)
 		{
 			printf("%d",matrix[m][n]);
-			printf("Go\n");
+			//printf("Go");
 
 		}
 		printf("\n");
-		printf("Go2\n");
+		//printf("Go2\n");
 
 	}
 
+
+	for (int m = 1; m <= noOfWords; m++)
+	{
+
+		
+			printf("%s",words[m]);
+			//printf("GO1\n");
+
+		
+		//printf("\n");
+
+	}
 
 
 }
